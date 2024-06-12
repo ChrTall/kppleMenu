b23deae71d33a95273387ca9cd84eab9cb69dcc6
ChrTall <chrtall@gmx.de>
2024-06-12 17:44:40 +0200

    WIP Fixed Settings, but icon picker does not work atm.

--- reinstall-plasmoid.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#  SPDX-FileCopyrightText: 2024 Christian Tallner <chrtall@gmx.de>
-#
-#  SPDX-License-Identifier: GPL-2.0-or-later
-kpackagetool5 -t "Plasma/Applet" -r package/
-kpackagetool5 -t "Plasma/Applet" -i package/
